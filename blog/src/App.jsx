7544f615fdd6c9c02fe6b58b6a167db8949c3296
sunjong2 <saysaysay223@gmail.com>
2024-07-21 11:09:09 +0900

    [FE] feat : 다른 UI로 변경

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
 import Header from "./component/Header";
 import MyInfo from "./component/MyInfo";
 function App() {
-  
+  const [position, setPosition] = useState(0);
+  function onScroll(){
+    setPosition(window.scrollY);
+    //headBg.current.style.setProperty('background-position', position*0.3)
+}
+//useEffect 두번째 매개변수의 값이 바뀔경우 첫번째 함수가 실행된다. 
+  useEffect(()=>{
+    window.addEventListener("scroll", onScroll);
+    return () =>{
+        window.removeEventListener("scroll", onScroll);
+    }
+},[position])
   return (
     <div>
-      <Header/>
+      <Header />
       <MyInfo/>
     </div>
   );
