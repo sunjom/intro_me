4572f494a9f79006ee2f202b3baf53eaf5062b29
sunjom <saysaysay223@naver.com>
2024-07-29 18:09:43 +0900

    [FE] feat : Footer main + 전체 페이지 환경 구축

---
+++ blog/src/component/main/Footer/FooterContent.jsx
@@ -0,0 +1,9 @@
+import Box from '../Box'
+import '../MainCss.css'
+export default function FooterContent(){
+    return(
+        <div className="MainCommon">
+            <Box title="Skill"></Box>
+        </div>
+    )
+}
